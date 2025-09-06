cc64c11a94983f591430830923066ebf1e2897af
juwenzhang <3137536916@qq.com>
2025-09-06 13:33:28 +0800

    feat

--- rspress.config.ts
+++ rspress.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   // 显示指定部署使用的 cdn 链接
   builderConfig: {
     output: {
-      assetPrefix: 'https://juwenzhang.github.io/android-study',
+      // assetPrefix: 'https://juwenzhang.github.io',
     }
   },
   // 指定需要进行部署的子路径
